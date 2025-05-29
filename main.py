6bdb23229bea8330993dad071ebb61ccfa841619
Commander SRS <shunzab.asad@gmail.com>
2025-05-29 08:39:04 +0500

    Changes to time function

--- main.py
+++ main.py
@@ -3,19 +3,23 @@ import datetime
 today = datetime.date.today()
 print(f"Today's date is: {today}")
 
-csv_file.get_csv()
+
 
 def view_data():
     try:
-        data = csv_file.get_csv()
-        print(data)
+        csv_file.get_csv()
+        data =pd.read_csv(csv_file.CSV_FILE)
+        if data.empty:
+            print("No data available.")
+        else:
+            print("Current data in the CSV file:")
+            print(data)
     except Exception as error:
-        print(f"An error occurred: {error}")
-
+        print(f"An error occurred while viewing data: {error}")
 
 def add_data():
     try:
-        date = input("Please enter the date (DD-MM-YYYY): ")
+        date = date_call()
         amount = input("Please enter the amount: ")
         category = input("Please enter the category of the amount: ")
         use = input("Please enter the use of the amount : ")
@@ -25,7 +29,20 @@ def add_data():
     except Exception as error:
         print(f"An error occurred: {error}")
 
-# def date():
+def date_call():
+    try:
+        date = input("Please enter the date (DD-MM-YYYY): ")
 
+        datetoday = datetime.datetime.strptime(date, "%d-%m-%Y").date()
+        if datetoday > today: # check that date is not in the future
+            print("The date cannot be in the future.")
+            
+            return date_call()
+        else:
+            pass
 
+    except ValueError as invformat: # invalid date format
+        print(f"Invalid date format: {invformat}")
+        return date_call()
 
+view_data()
